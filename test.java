2107eccd27981d79d7e6a0a828bd568774551d31
ayush <garg30844@gmail.com>
2025-11-17 13:07:54 +0530

    lecure 4

--- test.java
+++ test.java
@@ -1,5 +1,28 @@
 public class test{
     public static void main(String[] args) {
+        System.out.println("hello coder!");
+        Abc a=new Abc(2, 3);
+    System.out.println(a.add());
+    }
+}
+class Abc{
+    int x=0,y=0;
+    Abc(int x,int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    public int add()
+    {
+        return this.x+this.y;
+    }
+    public static void main(String[] args) {
         System.out.println("hello world!");
+    }
+}
+class Hello{
+    public void sayHello()
+    {
+        System.out.println("hello");
     }
 }
